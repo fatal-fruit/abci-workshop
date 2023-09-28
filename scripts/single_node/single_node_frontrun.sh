42a27093ceb030325126f535de58ec79e883d667
lg <lg@glnro.com>
2023-09-28 13:40:59 +0200

    Add single setup
    Signed-off-by: lg <lg@glnro.com>

---
+++ scripts/single_node/single_node_frontrun.sh
@@ -0,0 +1,4 @@
+binary=./build/cosmappd
+home=$HOME/.cosmappd
+
+$binary tx ns reserve "bob.cosmos" $($binary keys show alice -a --home $home --keyring-backend test) 1000uatom --from $($binary keys show bob -a --home $home --keyring-backend test)  --home $home -y
